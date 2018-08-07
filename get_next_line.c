3fe2a9a64c1d058c503b0c643f310110d064fe76
Pierre-quentin Deguingand <pdeguing@e1z3r5p1.42.us.org>
2018-08-07 16:22:58 -0700

    Updating recursion branch before trying another implementation

--- get_next_line.c
+++ get_next_line.c
@@ -6,45 +6,81 @@
 /*   By: pdeguing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/03 21:49:22 by pdeguing          #+#    #+#             */
-/*   Updated: 2018/08/07 08:51:45 by pdeguing         ###   ########.fr       */
+/*   Updated: 2018/08/07 14:43:53 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	create_array(char *buf, char **array)
-{
-	array = ft_strsplit(buf, '\n');
-}
+/*
+asdfasdfasd\nasdfasdfasd\n
+while (str[i] != '\n')
+	i++;
+	*line = ft_strsub(str, 0, i);
+	str = ft_strsub(str, i - 1, i);
+while (ret = read(fd, buf, BUFF_SIZE)
+buf[ret] = '\0';
+str = ft_strjoin(str, buf);
+	*/
 
 int		get_next_line(const int fd, char **line)
 {
 	static char	**array;
-
-	array = NULL;
-	if (array == NULL || ft_strlen(array) == 1)
+	char		*buf;
+	char		*tmp;
+	int			ret;
+	
+	ft_putstr("get_next_line() start\n");
+	if (!(buf = (char *)malloc(sizeof(char) * (BUF_SIZE + 1))))
+		return (-1);
+	ret = 0;
+	if (array == NULL || array[1] == NULL)
 	{
-		READ;
+		if (!(ret = read(fd, buf, BUF_SIZE)))
+			return (-1);
+//		ft_putnbr(ret);
+//		ft_putchar('\n');
+		buf[ret] = '\0';
+//		ft_putstr(buf);
 		if (array == NULL)
 			array = ft_strsplit(buf, '\n');
-		else if (ft_strlen(array) == 1)
+		else if (array[1] == NULL)
 		{
+			ft_putstr("else if\n");
 			tmp = array[0];
+//			ft_putstr(array[0]);
+//			ft_putchar('\n');
 			array = ft_strsplit(buf, '\n');
-			array[0] = ft_strjoin(tmp, array[0]);
+//			ft_putstr(array[0]);
+//			ft_putchar('\n');
+			if (array[0])
+			{
+//				ft_putstr("calling ft_strjoin()\n");
+				array[0] = ft_strjoin(tmp, array[0]);
+//				ft_putstr(array[0]);
+//				ft_putchar('\n');
+				ft_strdel(&tmp);
+			}
+		}
+//		ft_putstr("recursion call\n");
+		if (array)
+		{
+			ft_putstr(array[0]);
+			ft_putchar('\n');
 		}
 		get_next_line(fd, line);
 	}
-	else if (ft_strlen(array) > 1)
-	{
-		line = ft_strdup(array[0]);
-		ft_strdel(array[0]);
-		array = array + 1;
-		return (1);
-	}
-	return (0);
+//	ft_putstr(array[0]);
+//	ft_putchar('\n');
+//	if (array[1])
+//		ft_putstr("oops\n");
+	ft_putstr("duplicating into *line\n");
+	*line = ft_strdup(array[0]);
+	ft_putstr("duplicated into *line\n");
+	ft_strdel(array);
+	array++;
+	ft_putstr("get_next_line() end\n");
+	return (1);
 }
 
-// add failing conditions
-// add freeing of all memory
-// replace pseudo-code
+// got to make it work when there is no \n within BUF_SIZE
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pdeguing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/04 11:16:16 by pdeguing          #+#    #+#             */
-/*   Updated: 2018/08/06 12:31:52 by pdeguing         ###   ########.fr       */
+/*   Updated: 2018/08/07 12:55:29 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,18 @@
 
 int		main(int argc, char **argv)
 {
-	int		i;
 	int		fd;
-	char	**line;
+	char	*line;
 
 	if (argc != 2)
 		return (0);
 	fd = open(argv[1], O_RDONLY);
 	if (!fd)
 		return (-1);
-	if(!(line = (char **)malloc(sizeof(char *) * 11)))
-		return (-1);
-	line[10] = NULL;
-	i = 0;
-	ft_putstr("fsociety\n");
-	get_next_line(fd, line);
-	ft_putstr("fsociety\n");
-	ft_putstr(line[i]);
+	line = NULL;
+//	ft_putstr("fsociety\n");
+	get_next_line(fd, &line);
+	ft_putstr(line);
 	ft_putchar('\n');
 	return (0);
 }
