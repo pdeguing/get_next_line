73ff91fbbdf3b2b7a298ab925c07cf402c52e1c8
Pierre-quentin Deguingand <pdeguing@e1z2r3p27.42.us.org>
2018-08-07 08:38:31 -0700

    Initial commit for recursion version

--- get_next_line.c
+++ get_next_line.c
@@ -6,52 +6,45 @@
 /*   By: pdeguing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/03 21:49:22 by pdeguing          #+#    #+#             */
-/*   Updated: 2018/08/06 12:31:49 by pdeguing         ###   ########.fr       */
+/*   Updated: 2018/08/07 08:37:42 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+void	create_array(char *buf, char **array)
+{
+	array = ft_strsplit(buf, '\n');
+}
+
 int		get_next_line(const int fd, char **line)
 {
-	static int		i;
-	int				j;
-	int				ret;
-	char			*buf;
-	char			*tmp;
-	int				new_line;
-	
-	if (!(buf = (char *)malloc(sizeof(char) * (BUF_SIZE + 1))))
-		return (-1);
-	new_line = 0;
-	while (new_line == 0)
+	static char	**array;
+
+	array = NULL;
+	if (array == NULL || ft_strlen(array) == 1)
 	{
-		ret = read(fd, buf, BUF_SIZE);
-		if (ret == 0)
-			return (0);
-		buf[ret] = '\0';
-		j = 0;
-		while (buf[j] && new_line == 0)
+		READ;
+		if (array == NULL)
+			create_array(buf, array);
+		else if (ft_strlen(array) == 1)
 		{
-			if(buf[j] == '\n')
-			{
-				tmp = ft_strncpy(tmp, buf, j);
-				ft_strclr(&buf[j]);	
-				new_line = 1;
-			}
-			j++;
+			tmp = array[0];
+			create_array(buf, array);
+			array[0] = ft_strjoin(tmp, array[0]);
 		}
-		i = 0;
-		ft_putstr(buf);
-		ft_putchar('\n');
-		line[i] = buf;
-		ft_putstr(line[i]);
-		ft_putchar('\n');
-		ft_putchar('\n');
-//		if (!new_line)
-//		{
-//			get_next_line(fd, line + 1);
-//		}
+		get_next_line(fd, line);
 	}
-	return (1);
+	else if (ft_strlen(array) > 1)
+	{
+		line = ft_strdup(array[0]);
+		ft_strdel(array[0]);
+		array = array + 1;
+		return (1);
+	}
+	return (0);
 }
+
+// add failing conditions
+// add freeing of all memory
+// replace pseudo-code
